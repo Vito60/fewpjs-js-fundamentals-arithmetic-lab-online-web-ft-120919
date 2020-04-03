4d7b238c51fce2d54d77e498a30f4d27e6efbbda
Vito60 <randallw1996@gmail.com>
2020-04-03 15:39:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+// function multiply(num1, num2) {
+//     let result = num1 * num2
+//     return result 
+// }
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
+
+let random = (Math.random() > 0) + 1; 
+
+let mod = 24 % 5
+
+let max = Math.max(1, 2 ,3 ,4 ,20)
